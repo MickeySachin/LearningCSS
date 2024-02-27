58165c8aa37305e440ff6aeba5b70fc1797c8b49
Sachin <61985738+MickeySachin@users.noreply.github.com>
2024-02-27 06:13:22 +0530

    Add files via upload

---
+++ vixen.com/script.js
@@ -0,0 +1,19 @@
+
+// let selector1 = document.getElementById('#selc1').id;
+// let selector2 = document.querySelectorAll('selc2');
+// let selector3 = document.querySelectorAll('selc3');
+// let selector4 = document.querySelectorAll('selc4');
+
+let selectedCard = document.getElementById('card-img');
+
+// console.log(selector1)
+// selector1.onclick = () => {
+
+//     selectedCard.src = 'b.jpg';
+// }
+
+// let selector1 = document.getElementsByTagName('div');
+// console.log(selector1['selc1'].onclick = () =>     
+//     selectedCard.src = 'b.jpg'
+)
+
